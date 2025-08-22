e6085f8aa6f61a6e4fdcd12b16672d98624606d6
Andresaprendes <andreseduspanish@gmail.com>
2025-08-22 10:53:38 -0500

    feat(ads): configure Google Ads (AW-17417625713) alongside GA4 and track SPA page views

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -8,6 +8,7 @@ declare global {
 }
 
 const GA_MEASUREMENT_ID = 'G-GC21QKFV8K'
+const ADS_MEASUREMENT_ID = 'AW-17417625713'
 
 export default function Analytics() {
 	const location = useLocation()
@@ -15,6 +16,9 @@ export default function Analytics() {
 	useEffect(() => {
 		if (!window.gtag) return
 		window.gtag('config', GA_MEASUREMENT_ID, {
+			page_path: location.pathname + location.search,
+		})
+		window.gtag('config', ADS_MEASUREMENT_ID, {
 			page_path: location.pathname + location.search,
 		})
 	}, [location])
